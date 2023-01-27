2cfe5cabe176f7257142ca699629247ba0110a58
Cory <maurbeus@yahoo.com>
2023-01-26 22:14:00 -0800

    Edit app.js

--- app.js
+++ app.js
@@ -28,6 +28,4 @@ app.get('/futureProjects', (req, res) => {
     res.sendFile(__dirname + '/futureProjects.html');
 });
 
-app.listen(3000, (err, res) => {
-    console.log("Server running on port 3000");
-});
+app.listen(process.env.PORT || 5000);
