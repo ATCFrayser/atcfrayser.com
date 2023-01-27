94c50446b5cc27d488320e6a59bc85565b0e8753
Cory <maurbeus@yahoo.com>
2023-01-26 21:27:21 -0800

    Add pages and images

--- app.js
+++ app.js
@@ -6,18 +6,26 @@ const app = express();
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
-    const url = "https://api.openweathermap.org/data/2.5/weather?lat=39.7392&lon=-104.9903&appid=32d0e9db714ebb10af4f1549705568dd";
+    // const url = "https://api.openweathermap.org/data/2.5/weather?lat=39.7392&lon=-104.9903&appid=32d0e9db714ebb10af4f1549705568dd";
     
-    https.get(url,
-    (response) => {
-        console.log(response.statusCode);
+    // https.get(url,
+    // (response) => {
+    //     console.log(response.statusCode);
 
-        response.on('data', (data) => {
-            const weatherData = JSON.parse(data);
-            console.log(weatherData);
-        });
-    });
+    //     response.on('data', (data) => {
+    //         const weatherData = JSON.parse(data);
+    //         console.log(weatherData);
+    //     });
+    // });
     res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/thisWebsite', (req, res) => {
+    res.sendFile(__dirname + '/thisWebsite.html');
+});
+
+app.get('/futureProjects', (req, res) => {
+    res.sendFile(__dirname + '/futureProjects.html');
 });
 
 app.listen(3000, (err, res) => {
