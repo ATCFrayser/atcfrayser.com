61c096c1df5700da67d091b8aa4eb140b90618dc
atcfrayser <maurbeus@yahoo.com>
2023-02-02 20:57:28 -0800

    Fix Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,8 +11,6 @@ function Footer() {
     <img src='assets/images/mountainNarrow.png' className="img-fluid" alt="Mount Elbert" />
           <div class="row" style={{textAlign: "center"}}>
             <div class="col"><p>Copyright © {currentYear}</p></div>
-            <div class="col"><a href="#">Top of page</a></div>
-            <div class="col"><a href="about">About</a></div>
           </div>
     </footer>
     );
